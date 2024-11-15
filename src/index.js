b8a66cc9cfdeb79892eb3d8dcb6f4b719fb7bb0e
Bernard <bbarqueroc@gmail.com>
2024-11-15 16:29:48 -0600

    Solved port

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ import app from "./app.js";
 import {PORT} from "./config.js";
 
 
-app.listen(PORT); // ANTES ERA 3000 dev , en prod es PORT
+app.listen(PORT,"0.0.0.0"); // ANTES ERA 3000 dev , en prod es PORT
 console.log("server running on port", PORT);
