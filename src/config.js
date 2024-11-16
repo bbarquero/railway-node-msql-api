b3f8c6b61a3cd802af3c0d378c12b90afe4fc870
Bernard <bbarqueroc@gmail.com>
2024-11-16 10:30:57 -0600

    Search Port

--- src/config.js
+++ src/config.js
@@ -18,7 +18,7 @@ export const DB_PASSWORD = process.env.DB_PASSWORD || "LocalSion25.";
 export const DB_HOST = process.env.DB_HOST || "Localhost";
 export const DB_DATABASE = process.env.DB_DATABASE || "companydb";
 
-//Puerto base datos,
+//Puerto base datos,3306
 export const DB_PORT = process.env.DB_PORT || 3306;
 
 //
--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import app from "./app.js";
 import {PORT} from "./config.js";
 
 
-app.listen(PORT,"0.0.0.0"); // ANTES ERA 3000 dev , en prod es PORT
+app.listen(PORT);
+//app.listen(PORT,"0.0.0.0"); // ANTES ERA 3000 dev , en prod es PORT
 console.log("server running on port", PORT);
